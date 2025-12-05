25f105780e989d00e0252f784e1068a2b72b8785
tb-tian <thienan020405@gmail.com>
2025-12-05 20:57:29 +0700

    feat: add Movie model

---
+++ server/models/Movie.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Movie = sequelize.define('Movie', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  director: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  actor: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  genre: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  duration: {
+    type: DataTypes.INTEGER, // Duration in minutes
+    allowNull: false
+  },
+  releaseDate: {
+    type: DataTypes.DATEONLY, // Date
+    allowNull: false
+  },
+  posterUrl: {
+    type: DataTypes.STRING, // poster link
+    allowNull: true
+  },
+  trailerUrl: {
+    type: DataTypes.STRING, // trailer link
+    allowNull: true
+  },
+  status: {
+    type: DataTypes.ENUM('showing', 'coming_soon', 'ended'),
+    defaultValue: 'coming_soon'
+  }
+});
+
+module.exports = Movie;
