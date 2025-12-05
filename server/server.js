a7c9eaf339d9468e701648e562640f2dfd83b9ae
tb-tian <thienan020405@gmail.com>
2025-12-05 20:57:38 +0700

    feat: import Movie and Cinema models in server configuration

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const cors = require("cors")
 const dotenv = require('dotenv');
 const db = require("./config/db")
 const User = require('./models/User');
+const Movie = require('./models/Movie');
+const Cinema = require('./models/Cinema');
 
 dotenv.config()
 
