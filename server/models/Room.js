4579b8757a355fc95b168d7c0b134917fc72ae9f
tb-tian <thienan020405@gmail.com>
2025-12-05 21:28:13 +0700

    feat: add Room model

---
+++ server/models/Room.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Room = sequelize.define('Room', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false // Room 01
+  },
+  type: {
+    type: DataTypes.ENUM('2D', '3D', 'IMAX'),
+    defaultValue: '2D'
+  },
+  totalSeats: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+});
+
+module.exports = Room;
