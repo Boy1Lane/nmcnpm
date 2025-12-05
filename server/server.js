4b8da82c666226b49cead700301dae6247cdda66
tb-tian <thienan020405@gmail.com>
2025-12-06 00:06:56 +0700

    feat: establish relationships for Showtime, Seat, and ShowtimeSeat models

--- server/server.js
+++ server/server.js
@@ -8,18 +8,36 @@ const Cinema = require('./models/Cinema');
 const Room = require('./models/Room');
 const Seat = require('./models/Seat');
 const Showtime = require('./models/Showtime');
+const ShowtimeSeat = require('./models/ShowtimeSeat');
 
+// Relationship: A Cinema has many Rooms
 Cinema.hasMany(Room, { foreignKey: 'cinemaId' });
 Room.belongsTo(Cinema, { foreignKey: 'cinemaId' });
 
+// Relationship: A Room has many physical Seats
 Room.hasMany(Seat, { foreignKey: 'roomId' });
 Seat.belongsTo(Room, { foreignKey: 'roomId' });
 
+// Relationship: A Movie has many Showtimes
 Movie.hasMany(Showtime, { foreignKey: 'movieId' });
 Showtime.belongsTo(Movie, { foreignKey: 'movieId' });
 
+// Relationship: A Room hosts many Showtimes
 Room.hasMany(Showtime, { foreignKey: 'roomId' });
 Showtime.belongsTo(Room, { foreignKey: 'roomId' });
+
+// Relationship: Many-to-Many between Showtime and Seat
+// Connects specific showtimes to physical seats via the ShowtimeSeat junction table
+Showtime.belongsToMany(Seat, { through: ShowtimeSeat, foreignKey: 'showtimeId' });
+Seat.belongsToMany(Showtime, { through: ShowtimeSeat, foreignKey: 'seatId' });
+
+// Super Many-to-Many: Direct associations to the junction table (ShowtimeSeat)
+// Allows querying the status (booked/available) of a seat for a specific showtime directly
+Showtime.hasMany(ShowtimeSeat, { foreignKey: 'showtimeId' });
+ShowtimeSeat.belongsTo(Showtime, { foreignKey: 'showtimeId' });
+
+Seat.hasMany(ShowtimeSeat, { foreignKey: 'seatId' });
+ShowtimeSeat.belongsTo(Seat, { foreignKey: 'seatId' });
 
 dotenv.config()
 
@@ -49,5 +67,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
