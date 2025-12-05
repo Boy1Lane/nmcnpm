1413aebfd6ddcb42c6bcd24c517b6f0af7475335
tb-tian <thienan020405@gmail.com>
2025-12-06 00:06:49 +0700

    feat: create ShowtimeSeat model with status, price, and locking functionality

---
+++ server/models/ShowtimeSeat.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const ShowtimeSeat = sequelize.define('ShowtimeSeat', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  status: {
+    type: DataTypes.ENUM('AVAILABLE', 'LOCKED', 'SOLD'),
+    defaultValue: 'AVAILABLE'
+  },
+  price: {
+    type: DataTypes.INTEGER, // Final price
+    allowNull: false
+  },
+  lockedAt: {
+    type: DataTypes.DATE, // Time started to lock seat
+    allowNull: true
+  }
+  // showtimeID and seatID to be add 
+});
+
+module.exports = ShowtimeSeat;
