2e6e90cea2178d0d66e29a2dc3c1b20895fd3b6a
tb-tian <thienan020405@gmail.com>
2025-12-05 21:52:17 +0700

    feat: add Showtime model

---
+++ server/models/Showtime.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Showtime = sequelize.define('Showtime', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  startTime: {
+    type: DataTypes.DATE, // Day and time
+    allowNull: false
+  },
+  endTime: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  basePrice: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+module.exports = Showtime;
