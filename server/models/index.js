c883a5b70d58fc17a0356dfee25961d0b837d99c
tb-tian <thienan020405@gmail.com>
2025-12-07 02:01:02 +0700

    refactor: move model relationships to index.js for better organization

---
+++ server/models/index.js
@@ -0,0 +1,48 @@
+const sequelize = require('../config/db');
+const User = require('./User');
+const Movie = require('./Movie');
+const Cinema = require('./Cinema');
+const Room = require('./Room');
+const Seat = require('./Seat');
+const Showtime = require('./Showtime');
+const ShowtimeSeat = require('./ShowtimeSeat');
+
+// Relationship: A Cinema has many Rooms
+Cinema.hasMany(Room, { foreignKey: 'cinemaId' });
+Room.belongsTo(Cinema, { foreignKey: 'cinemaId' });
+
+// Relationship: A Room has many physical Seats
+Room.hasMany(Seat, { foreignKey: 'roomId' });
+Seat.belongsTo(Room, { foreignKey: 'roomId' });
+
+// Relationship: A Movie has many Showtimes
+Movie.hasMany(Showtime, { foreignKey: 'movieId' });
+Showtime.belongsTo(Movie, { foreignKey: 'movieId' });
+
+// Relationship: A Room hosts many Showtimes
+Room.hasMany(Showtime, { foreignKey: 'roomId' });
+Showtime.belongsTo(Room, { foreignKey: 'roomId' });
+
+// Relationship: Many-to-Many between Showtime and Seat
+// Connects specific showtimes to physical seats via the ShowtimeSeat junction table
+Showtime.belongsToMany(Seat, { through: ShowtimeSeat, foreignKey: 'showtimeId' });
+Seat.belongsToMany(Showtime, { through: ShowtimeSeat, foreignKey: 'seatId' });
+
+// Super Many-to-Many: Direct associations to the junction table (ShowtimeSeat)
+// Allows querying the status (booked/available) of a seat for a specific showtime directly
+Showtime.hasMany(ShowtimeSeat, { foreignKey: 'showtimeId' });
+ShowtimeSeat.belongsTo(Showtime, { foreignKey: 'showtimeId' });
+
+Seat.hasMany(ShowtimeSeat, { foreignKey: 'seatId' });
+ShowtimeSeat.belongsTo(Seat, { foreignKey: 'seatId' });
+
+module.exports = {
+    sequelize,
+    User,
+    Movie,
+    Cinema,
+    Room,
+    Seat,
+    Showtime,
+    ShowtimeSeat
+};
--- server/server.js
+++ server/server.js
@@ -2,42 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require('dotenv');
 const db = require("./config/db")
-const User = require('./models/User');
-const Movie = require('./models/Movie');
-const Cinema = require('./models/Cinema');
-const Room = require('./models/Room');
-const Seat = require('./models/Seat');
-const Showtime = require('./models/Showtime');
-const ShowtimeSeat = require('./models/ShowtimeSeat');
-
-// Relationship: A Cinema has many Rooms
-Cinema.hasMany(Room, { foreignKey: 'cinemaId' });
-Room.belongsTo(Cinema, { foreignKey: 'cinemaId' });
-
-// Relationship: A Room has many physical Seats
-Room.hasMany(Seat, { foreignKey: 'roomId' });
-Seat.belongsTo(Room, { foreignKey: 'roomId' });
-
-// Relationship: A Movie has many Showtimes
-Movie.hasMany(Showtime, { foreignKey: 'movieId' });
-Showtime.belongsTo(Movie, { foreignKey: 'movieId' });
-
-// Relationship: A Room hosts many Showtimes
-Room.hasMany(Showtime, { foreignKey: 'roomId' });
-Showtime.belongsTo(Room, { foreignKey: 'roomId' });
-
-// Relationship: Many-to-Many between Showtime and Seat
-// Connects specific showtimes to physical seats via the ShowtimeSeat junction table
-Showtime.belongsToMany(Seat, { through: ShowtimeSeat, foreignKey: 'showtimeId' });
-Seat.belongsToMany(Showtime, { through: ShowtimeSeat, foreignKey: 'seatId' });
-
-// Super Many-to-Many: Direct associations to the junction table (ShowtimeSeat)
-// Allows querying the status (booked/available) of a seat for a specific showtime directly
-Showtime.hasMany(ShowtimeSeat, { foreignKey: 'showtimeId' });
-ShowtimeSeat.belongsTo(Showtime, { foreignKey: 'showtimeId' });
-
-Seat.hasMany(ShowtimeSeat, { foreignKey: 'seatId' });
-ShowtimeSeat.belongsTo(Seat, { foreignKey: 'seatId' });
+const { User, Movie, Cinema, Room, Seat, Showtime, ShowtimeSeat } = require('./models');
 
 dotenv.config()
 
