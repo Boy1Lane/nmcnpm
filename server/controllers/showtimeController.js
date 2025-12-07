5d707e799672bff49d510d44881af692bed5480f
tb-tian <thienan020405@gmail.com>
2025-12-07 17:37:01 +0700

    feat: add getSeatsByShowtime endpoint to retrieve seats for a specific showtime

--- server/controllers/showtimeController.js
+++ server/controllers/showtimeController.js
@@ -57,3 +57,30 @@ exports.createShowtime = async (req, res) => {
     }
 
 }
+
+exports.getSeatsByShowtime = async (req, res) => {
+  try {
+    const { showtimeId } = req.params;
+
+    const seats = await ShowtimeSeat.findAll({
+      where: { showtimeId },
+      include: [{
+        model: Seat,
+        attributes: ['row', 'number', 'type'] // Chỉ lấy Hàng, Số, Loại
+      }],
+      order: [
+        [Seat, 'row', 'ASC'], 
+        [Seat, 'number', 'ASC']
+      ]
+    });
+
+    if (!seats) {
+      return res.status(404).json({ message: 'No showtime was found' });
+    }
+
+    res.status(200).json(seats);
+  } catch {
+    console.error(error);
+    res.status(500).json({ message: 'Error when retrieving Seats by Showtime' });
+  }
+};
