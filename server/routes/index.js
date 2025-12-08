ea0fc64e3b2ccf87890c268d8dc7bd2221712bba
ndkhanhus27 <ndkhanh05lqdqt@gmail.com>
2025-12-08 20:07:41 +0700

    Save my work before merging main

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const adminRoutes = require('./admin/index');
 const apiRoutes = require('./api/index');
 const authRoutes = require('./authRoute');
+const authMiddleware = require('../middlewares/authMiddleware');
+const adminMiddleware = require('../middlewares/adminMiddleware');
 
 function Routes(app) {
     app.use('/api', apiRoutes);
-    app.use('/admin', adminRoutes);
+    app.use('/admin', authMiddleware, adminMiddleware, adminRoutes);
     app.use('/auth', authRoutes);
     app.get('/', (req, res) => {
         res.send('Xin chào! Server CinemaVerse đang chạy ổn định.');
