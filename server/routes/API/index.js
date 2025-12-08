2570bd83fa02d699daacea5870852341fa5d5a98
Trần Thanh Đạt <149404063+tdat1465@users.noreply.github.com>
2025-12-08 21:22:10 +0700

    Delete server/routes/API/index.js

--- server/routes/API/index.js
+++
@@ -1,8 +0,0 @@
-const movieRoutes = require('./movieRoute');
-const showtimeRoutes = require('./showtimeRoute');
-const router = require('express').Router();
-
-router.use('/movies', movieRoutes);
-router.use('/showtimes', showtimeRoutes);
-
-module.exports = router;
