7db7a82536df78ae4529866474c8430f70d68d30
tdat1465 <dat1405012005@gmail.com>
2025-12-09 22:20:47 +0700

    feat: add user in admin api

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,10 +1,20 @@
 const movieRoutes = require('./movieRoute.js');
-const adminController = require('../../controllers/admin/adminController');
+const roomRoutes = require('./roomRoute.js');
+const showtimeRoutes = require('./showtimeRoute.js');
 const router = require('express').Router();
 
 // /admin/movies -> movieRoutes
 router.use('/movies', movieRoutes);
-// /admin/set-role -> setUserRole
-router.post('/set-role', adminController.setUserRole);
+
+// /admin/rooms -> roomRoutes
+router.use('/rooms', roomRoutes);
+
+// /admin/showtimes -> showtimeRoutes
+router.use('/showtimes', showtimeRoutes);
+
+// /admin 
+router.get('/', (req, res) => {
+  res.json({ message: 'Admin Home' });
+});
 
 module.exports = router;
