84f001722528433b52ccd74317897839d654bbe0
tb-tian <thienan020405@gmail.com>
2025-12-18 22:57:07 +0700

    feat: add getMovieById endpoint to movieController and movieRoute

--- server/controllers/user/movieController.js
+++ server/controllers/user/movieController.js
@@ -8,3 +8,19 @@ exports.getAllMovies = async (req, res) => {
     res.status(500).json({ message: 'getAllMovies error' });
   }
 };
+
+exports.getMovieById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findByPk(id);
+
+    if (!movie) {
+      return res.status(404).json({ message: 'Movie not found' });
+    }
+
+    res.status(200).json(movie);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
--- server/routes/api/movieRoute.js
+++ server/routes/api/movieRoute.js
@@ -5,4 +5,7 @@ const movieController = require('../../controllers/user/movieController');
 // GET /api/movies -> getAllMovies
 router.get('/', movieController.getAllMovies);
 
+// GET /api/movies/:id -> getMovieById
+router.get('/:id', movieController.getMovieById);
+
 module.exports = router;
