358bf3f3b94993c7a2204a555288f2f6fc044613
ndkhanhus27 <ndkhanh05lqdqt@gmail.com>
2026-01-03 00:09:24 +0700

    fix: fix UI for sideMenu

--- client/src/layout/AdminLayout.jsx
+++ client/src/layout/AdminLayout.jsx
@@ -1,18 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Admin/Header.jsx";
-import Footer from "../components/Admin/Footer.jsx";
 import SideMenu from "../components/Admin/SideMenu.jsx";
 import "../styles/Admin/AdminLayout.css";
+
 export default function AdminLayout() {
   return (
-    <div className="layout-container">
-      <Header />
+    <div className="admin-layout">
+      {/* 1. Menu bên trái (Cố định full chiều cao) */}
+      <SideMenu />
 
-      <div className="layout-main">
-        <SideMenu />
+      {/* 2. Wrapper bên phải (Header + Nội dung) */}
+      <div className="admin-main-wrapper">
+        <Header />
 
-        <div className="layout-content">
-          {/* ⭐ Đây là nơi render page con (phần tím của bạn) */}
+        <div className="admin-content">
           <Outlet />
         </div>
       </div>
